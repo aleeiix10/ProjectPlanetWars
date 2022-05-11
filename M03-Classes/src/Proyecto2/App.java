83e9332197330d140956fbd560570fb9e4b5d30e
aleeiix10 <95906986+aleeiix10@users.noreply.github.com>
2022-05-11 13:11:30 +0200

    Aleix

--- M03-Classes/src/Proyecto2/App.java
+++
@@ -1,8 +0,0 @@
-package Proyecto2;
-
-
-public class App {
-	public static void main(String[] args) {
-		Conexion db=new Conexion("jdbc:oracle:thin:@192.168.40.2:1521:orcl","alumnoAMS5","alumnoAMS5");	
-	}
-}
