17257f88f50cb6bdb65eebd765dc9622c3f25976
aleeiix10 <95906986+aleeiix10@users.noreply.github.com>
2022-05-11 13:17:14 +0200

    Cambios parte m3

---
+++ M03-Classes/src/Proyecto2/Conexion.java
@@ -0,0 +1,35 @@
+package Proyecto2;
+
+import java.sql.*;
+import javax.swing.*;
+
+public class Conexion {
+	String user;
+	String pass;
+	String url;
+	Connection cn;
+	public Conexion(String url1, String user1, String pass1) {
+		user=user1;
+		pass=pass1;
+		url=url1;
+		conectar();
+		
+	}
+	public Connection getCn() {
+		return cn;
+	}
+	public void conectar() {
+		try {
+			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+      
+            cn = DriverManager.getConnection(url, user, pass);
+			System.out.println("Va de putos locos");
+			
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+			System.out.println("No va una mierda");
+		}
+	}
+	
+}
+
---
+++ M03-Classes/src/Proyecto2/IonCannon.java
@@ -0,0 +1,73 @@
+package Proyecto2;
+
+public class IonCannon extends Defense implements MilitaryUnit{
+	
+	int armadura_plus = Variables.PLUS_ARMOR_MISSILELAUNCHER_BY_TECHNOLOGY;
+	int dano_plus = Variables.PLUS_ATTACK_MISSILELAUNCHER_BY_TECHNOLOGY;
+	
+	IonCannon(int armor,int baseDamage){
+		this.baseDamage = baseDamage;
+		this.armor = armor;
+		this.initialArmor = armor;
+	}
+
+	@Override
+	public int attack() {
+		return this.baseDamage;
+	}
+
+	@Override
+	public void takeDamage(int receivedDamage) {
+		this.armor = this.armor - receivedDamage;
+	}
+
+	@Override
+	public int getActualArmor() {
+		return this.armor;
+	}
+
+	@Override
+	public int getMetalCost() {
+		return Variables.METAL_COST_IONCANNON;
+	}
+
+	@Override
+	public int getDeuteriumCost() {
+		return Variables.DEUTERIUM_COST_IONCANNON;
+	}
+
+	@Override
+	public int getChanceGeneratinWaste() {
+		int porcentaje=Variables.CHANCE_GENERATNG_WASTE_IONCANNON;
+		int num_random;
+		
+		num_random = (int)(Math.random()*101);
+		
+		if (num_random<=porcentaje) {
+			return 1;
+		}
+		
+		return 0;
+	}
+
+	@Override
+	public int getChanceAttackAgain() {
+		int porcentaje=Variables.CHANCE_ATTACK_AGAIN_IONCANNON;
+		int num_random;
+		
+		num_random = (int)(Math.random()*101);
+		
+		if (num_random<=porcentaje) {
+			return 1;
+		}
+		
+		return 0;
+	}
+
+	@Override
+	public void resetArmor() {
+		this.armor = this.initialArmor;
+	}
+	
+}
+
